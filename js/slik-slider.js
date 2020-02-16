b407dcaf6ea0e8f5bee45f9721a91a6f48fb898a
Skaifol <46871367+Skaifol@users.noreply.github.com>
2020-02-16 23:48:40 +0200

    init commit

---
+++ js/slik-slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('.ant-carousel-list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    prevArrow: "<img src='img/left-arrow-button.png' class='prev' alt='1'>",
+    nextArrow: "<img src='img/right-arrow-button.png' class='next' alt='2'>",
+  });
+});
