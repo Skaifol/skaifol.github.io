dd3c33e0ca01b28aa227c5a63dc6583e22324e14
Skaifol <46871367+Skaifol@users.noreply.github.com>
2020-02-16 23:40:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,139 @@
-const tabLinks = document.querySelectorAll(".tabs a"); const tabPanels = document.querySelectorAll(".tabs-panel"); for (let el of tabLinks) { el.addEventListener("click", e => { e.preventDefault(); document.querySelector(".tabs li.active").classList.remove("active"); document.querySelector(".tabs-panel.active").classList.remove("active"); const parentListItem = el.parentElement; parentListItem.classList.add("active"); const index = [...parentListItem.parentElement.children].indexOf(parentListItem); const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index); panel[0].classList.add("active") }) } 'use strict'; var multiItemSlider = (function () { return function (p, t) { var g = document.querySelector(p), k = g.querySelector('.slider__wrapper'), l = g.querySelectorAll('.slider__item'), q = g.querySelectorAll('.slider__control'), u = g.querySelector('.slider__control_left'), v = g.querySelector('.slider__control_right'), m = parseFloat(getComputedStyle(k).width), n = parseFloat(getComputedStyle(l[0]).width), h = 0, i = 0, o = n / m * 100, b = []; l.forEach(function (c, a) { b.push({ item: c, position: a, transform: 0 }) }); var d = { getItemMin: function () { var f = 0; b.forEach(function (c, a) { if (c.position < b[f].position) { f = a } }); return f }, getItemMax: function () { var f = 0; b.forEach(function (c, a) { if (c.position > b[f].position) { f = a } }); return f }, getMin: function () { return b[d.getItemMin()].position }, getMax: function () { return b[d.getItemMax()].position } }var j = function (c) { var a; if (c === 'right') { h++; if ((h + m / n - 1) > d.getMax()) { a = d.getItemMin(); b[a].position = d.getMax() + 1; b[a].transform += b.length * 100; b[a].item.style.transform = 'translateX(' + b[a].transform + '%)' } i -= o } if (c === 'left') { h--; if (h < d.getMin()) { a = d.getItemMax(); b[a].position = d.getMin() - 1; b[a].transform -= b.length * 100; b[a].item.style.transform = 'translateX(' + b[a].transform + '%)' } i += o } k.style.transform = 'translateX(' + i + '%)' }var r = function (c) { if (c.target.classList.contains('slider__control')) { c.preventDefault(); var a = c.target.classList.contains('slider__control_right') ? 'right' : 'left'; j(a) } }; var s = function () { q.forEach(function (c) { c.addEventListener('click', r) }) }s(); return { right: function () { j('right') }, left: function () { j('left') } } } }()); var slider = multiItemSlider('.slider')$(document).ready(function () { $('.ant-carousel-list').slick({ infinite: true, slidesToShow: 3, prevArrow: "<img src='img/left-arrow-button.png' class='prev' alt='1'>", nextArrow: "<img src='img/right-arrow-button.png' class='next' alt='2'>", }) }); $(document).ready(function () { $("#phone").mask("+7 (999) 999 99 99"); $("#phone2").mask("+7 (999) 999 99 99"); $("#phone3").mask("+7 (999) 999 99 99") });
+const tabLinks = document.querySelectorAll(".tabs a");
+const tabPanels = document.querySelectorAll(".tabs-panel");
+
+for (let el of tabLinks) {
+  el.addEventListener("click", e => {
+    e.preventDefault();
+
+    document.querySelector(".tabs li.active").classList.remove("active");
+    document.querySelector(".tabs-panel.active").classList.remove("active");
+
+    const parentListItem = el.parentElement;
+    parentListItem.classList.add("active");
+    const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+
+    const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
+    panel[0].classList.add("active");
+  });
+}
+
+'use strict';
+var multiItemSlider = (function () {
+  return function (selector, config) {
+    var
+      _mainElement = document.querySelector(selector), // основный элемент блока
+      _sliderWrapper = _mainElement.querySelector('.slider__wrapper'), // обертка для .slider-item
+      _sliderItems = _mainElement.querySelectorAll('.slider__item'), // элементы (.slider-item)
+      _sliderControls = _mainElement.querySelectorAll('.slider__control'), // элементы управления
+      _sliderControlLeft = _mainElement.querySelector('.slider__control_left'), // кнопка "LEFT"
+      _sliderControlRight = _mainElement.querySelector('.slider__control_right'), // кнопка "RIGHT" 
+      _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width), // ширина обёртки
+      _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width), // ширина одного элемента    
+      _positionLeftItem = 0, // позиция левого активного элемента
+      _transform = 0, // значение транфсофрмации .slider_wrapper
+      _step = _itemWidth / _wrapperWidth * 100, // величина шага (для трансформации)
+      _items = []; // массив элементов
+
+    // наполнение массива _items
+    _sliderItems.forEach(function (item, index) {
+      _items.push({ item: item, position: index, transform: 0 });
+    });
+
+    var position = {
+      getItemMin: function () {
+        var indexItem = 0;
+        _items.forEach(function (item, index) {
+          if (item.position < _items[indexItem].position) {
+            indexItem = index;
+          }
+        });
+        return indexItem;
+      },
+      getItemMax: function () {
+        var indexItem = 0;
+        _items.forEach(function (item, index) {
+          if (item.position > _items[indexItem].position) {
+            indexItem = index;
+          }
+        });
+        return indexItem;
+      },
+      getMin: function () {
+        return _items[position.getItemMin()].position;
+      },
+      getMax: function () {
+        return _items[position.getItemMax()].position;
+      }
+    }
+
+    var _transformItem = function (direction) {
+      var nextItem;
+      if (direction === 'right') {
+        _positionLeftItem++;
+        if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) > position.getMax()) {
+          nextItem = position.getItemMin();
+          _items[nextItem].position = position.getMax() + 1;
+          _items[nextItem].transform += _items.length * 100;
+          _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+        }
+        _transform -= _step;
+      }
+      if (direction === 'left') {
+        _positionLeftItem--;
+        if (_positionLeftItem < position.getMin()) {
+          nextItem = position.getItemMax();
+          _items[nextItem].position = position.getMin() - 1;
+          _items[nextItem].transform -= _items.length * 100;
+          _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+        }
+        _transform += _step;
+      }
+      _sliderWrapper.style.transform = 'translateX(' + _transform + '%)';
+    }
+
+    // обработчик события click для кнопок "назад" и "вперед"
+    var _controlClick = function (e) {
+      if (e.target.classList.contains('slider__control')) {
+        e.preventDefault();
+        var direction = e.target.classList.contains('slider__control_right') ? 'right' : 'left';
+        _transformItem(direction);
+      }
+    };
+
+    var _setUpListeners = function () {
+      // добавление к кнопкам "назад" и "вперед" обрботчика _controlClick для событя click
+      _sliderControls.forEach(function (item) {
+        item.addEventListener('click', _controlClick);
+      });
+    }
+
+    // инициализация
+    _setUpListeners();
+
+    return {
+      right: function () { // метод right
+        _transformItem('right');
+      },
+      left: function () { // метод left
+        _transformItem('left');
+      }
+    }
+
+  }
+}());
+
+var slider = multiItemSlider('.slider') 
+
+$(document).ready(function () {
+  $('.ant-carousel-list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    prevArrow: "<img src='img/left-arrow-button.png' class='prev' alt='1'>",
+    nextArrow: "<img src='img/right-arrow-button.png' class='next' alt='2'>",
+  });
+});
+$(document).ready(function () {
+        $("#phone").mask("+7 (999) 999 99 99");
+        $("#phone2").mask("+7 (999) 999 99 99");
+        $("#phone3").mask("+7 (999) 999 99 99")
+});
