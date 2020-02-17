c682fc9b929df29e8c3cf2431f6141df67f4c70c
Skaifol <46871367+Skaifol@users.noreply.github.com>
2020-02-17 23:10:37 +0200

    Delete slik-slider.js

--- js/slik-slider.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $('.ant-carousel-list').slick({
-    infinite: true,
-    slidesToShow: 3,
-    prevArrow: "<img src='img/left-arrow-button.png' class='prev' alt='1'>",
-    nextArrow: "<img src='img/right-arrow-button.png' class='next' alt='2'>",
-  });
-});
